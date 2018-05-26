cf7c83f948810ed1b75730da36ebf9000ab03d54
Killersongh <jajoux@hotmail.com>
2018-05-26 16:37:55 +0200

    Add Main.java a second line for increase the file

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		
 		System.out.println("Voici mon code pour le Tp sur Github !");
+		System.out.println("Ce code sert à afficher du texte a l'écran !");
 
 	}
 
